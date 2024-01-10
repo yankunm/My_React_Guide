9d138777ecf55245cade507881172da74f14bdba
Yankun Meng <yankun.meng25@gmail.com>
2024-01-10 14:08:29 -0500

    init

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Chicken from "./Chicken"
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Chicken />
+      <Chicken />
+      <Chicken />
+    </div>
+  );
+}
+
+export default App
---
+++ my-first-react-app/src/Chicken.jsx
@@ -0,0 +1,7 @@
+import "./Chicken.css";
+
+function Chicken() {
+    return <h1 className="Chicken">Bock Bock Bock!!</h1>;
+}
+
+export default Chicken;
